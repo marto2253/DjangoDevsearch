91be5a0b5dec0778d09a08212ac1268f47e87217
marto2253 <georgiev.martin1@gmail.com>
2022-05-02 20:10:58 +0300

    Configured Reviews and now users who are registered could leave up to one review per project apart from their own ones.

--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 # from django.http import HttpResponse
 from .models import Project
-from .forms import ProjectForm
+from .forms import ProjectForm, ReviewForm
 from django.contrib.auth.decorators import login_required
 # from django.db.models import Q
 from .utils import search_projects, paginate_projects
+from django.contrib import messages
 
 # Create your views here.
 
@@ -20,7 +21,23 @@ def projects(request):
 
 def project(request, pk):
     project_obj = Project.objects.get(id=pk)
-    return render(request, 'projects/project.html', {'project': project_obj})
+    form = ReviewForm()
+
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        review = form.save(commit=False)
+        review.project = project_obj
+        review.owner = request.user.profile
+        review.save()
+
+        project_obj.get_vote_count
+
+        messages.success(request, 'Your review was successfully submitted!')
+
+        return redirect('project', pk=project_obj.id)
+
+
+    return render(request, 'projects/project.html', {'project': project_obj, 'form': form})
 
 
 @login_required(login_url='login')
